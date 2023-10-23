a0b5b99f1f2c157dc70295227d2bba4ff558806c
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2023-10-23 11:42:26 +0800

    Add args

--- CBOW.py
+++ CBOW.py
@@ -1,3 +1,5 @@
+import argparse
+
 from comman.model import (CBOW)
 from comman.optimizer import Adam
 from comman.trainer import Trainer
@@ -5,43 +7,70 @@ from comman.util import (context_target, to_gpu, save, load)
 
 from dataset import ptb
 
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=100)
+    parser.add_argument('--window-size', type=int, default=1)
+    parser.add_argument('--hidden-size', type=int, default=100)
+    parser.add_argument('--save-file', type=str, default='data/train.model')
+
+    return parser.parse_args()
+
+
+def print_args(args):
+    for key, value in args.items():
+        print(f'{key}:{value}', end='  ')
+    print()
+
+
 if __name__ == '__main__':
-    # file = 'data/train.model'
-    # window_size = 1
-    # hidden_size = 100
-    # batch_size = 100
-    # goal_epoch = 1
-    #
-    # corpus, word_to_id, id_to_word = ptb.load_data('train')
-    # vocab_size = len(word_to_id)
-    # contexts, target = context_target(corpus, window_size)
-    # contexts, target = to_gpu(contexts), to_gpu(target)
-    #
-    # model = CBOW(vocab_size, hidden_size, window_size, corpus)
-    # optimizer = Adam()
-    # trainer = Trainer(model, optimizer)
-    #
-    # trainer.train(contexts, target, goal_epoch, batch_size)
-    # trainer.plot()
-    #
-    # save(file, model=model)
-    file = 'data/train.model'
-    lr = 0.00001
-    window_size = 3
-    hidden_size = 100
-    batch_size = 200
-    goal_epoch = 3
+    opt = parse_opt()
+    print_args(vars(opt))
+
+    lr = opt.lr
+    goal_epoch = opt.epochs
+    batch_size = opt.batch_size
+    window_size = opt.window_size
+    hidden_size = opt.hidden_size
+    save_file = opt.save_file
 
     corpus, word_to_id, id_to_word = ptb.load_data('train')
     vocab_size = len(word_to_id)
     contexts, target = context_target(corpus, window_size)
     contexts, target = to_gpu(contexts), to_gpu(target)
 
-    model = load(file)
+    model = CBOW(vocab_size, hidden_size, window_size, corpus)
     optimizer = Adam(lr=lr)
     trainer = Trainer(model, optimizer)
 
     trainer.train(contexts, target, goal_epoch, batch_size)
     trainer.plot()
 
-    save(file, model=model)
+    save(save_file, model=model)
+
+    # opt = parse_opt()
+    # print_args(vars(opt))
+    #
+    # lr = opt.lr
+    # goal_epoch = opt.epochs
+    # batch_size = opt.batch_size
+    # window_size = opt.window_size
+    # hidden_size = opt.hidden_size
+    # save_file = opt.save_file
+    #
+    # corpus, word_to_id, id_to_word = ptb.load_data('train')
+    # vocab_size = len(word_to_id)
+    # contexts, target = context_target(corpus, window_size)
+    # contexts, target = to_gpu(contexts), to_gpu(target)
+    #
+    # model = load(save_file)
+    # optimizer = Adam(lr=lr)
+    # trainer = Trainer(model, optimizer)
+    #
+    # trainer.train(contexts, target, goal_epoch, batch_size)
+    # trainer.plot()
+    #
+    # save(save_file, model=model)
